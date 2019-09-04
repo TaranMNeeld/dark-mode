27d84b6bfc0ba6ba69df97334cb8d55eedd9bb45
Taran <thetaranneeld@gmail.com>
2019-09-04 13:42:37 -0600

    useLocalStorage is created and exported

---
+++ src/hooks/useLocalStorage.jsx
@@ -0,0 +1,21 @@
+import React, { useSate } from "react";
+
+const useLocalStorage = (key, initialValue) => {
+    
+    //Item is set to the desired key/value pair that was previously stored.
+    const storedItem = JSON.parse(localStorage.getItem(key));
+    
+    //Set storedValue equal to storedItem, if it exists within localStorage
+    //If storedItem does not already exist, then set storedValue equal to initialValue.
+    const [storedValue, setStoredValue] = useState(storedItem || initialValue);
+
+    //
+    const setValue = value => {
+        window.localStorage.setItem(key, JSON.stringify(value));
+        setStoredValue(value);
+    };
+
+    return [storedValue, setValue];
+}
+
+export default useLocalStorage;
